c46ebd0f1a81986c84c8d699cac8d99e87113e61
Frenzy265 <klaus_franziska@gmx.de>
2020-11-16 11:07:47 +0100

    Add set, read, delete function and dotenv

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
-const { readCommandLineArguments } = require("./lib/commandLine");
-const { getPassword } = require("./lib/passwords");
-const { askForMasterPassword } = require("./lib/questions");
+require("dotenv").config();
+const {
+  setPassword,
+  getPassword,
+  deletePasswordByName,
+} = require("./lib/passwords");
+const {
+  askForMasterPassword,
+  askForNext,
+  newPassword,
+  readPassword,
+  passwordToDelete,
+} = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
+const { connect, close } = require("./lib/database");
 
 async function run() {
+  console.log("Connection to database...");
+  await connect(process.env.DB_URL, process.env.DB_NAME);
+  console.log("Connectet to database!");
+
   const masterPassword = await askForMasterPassword();
 
-  if (!isMasterPasswordCorrect(masterPassword)) {
+  if (!(await isMasterPasswordCorrect(masterPassword))) {
     console.error("You are not welcome here! 👿 Try again!");
     return run();
   }
 
-  const [passwordName, newPasswordValue] = readCommandLineArguments();
-  if (!passwordName) {
-    console.error("Missing password name!");
-    return process.exit(9);
+  const nextToDo = await askForNext();
+  console.log(nextToDo);
+
+  if (nextToDo === "set") {
+    const [newPasswordName, newPasswordValue] = await newPassword();
+    await setPassword(newPasswordName, newPasswordValue);
+    console.log(`Password ${newPasswordName} set 🎉`);
   }
 
-  if (newPasswordValue) {
-    await setPassword(passwordName, newPasswordValue);
-    console.log(`Password ${passwordName} set 🎉`);
-  } else {
+  if (nextToDo === "read") {
+    const passwordName = await readPassword();
     const passwordValue = await getPassword(passwordName);
     console.log(`Your password is ${passwordValue} 🎉`);
   }
+
+  if (nextToDo === "delete") {
+    const deletedPassword = await passwordToDelete();
+    await deletePasswordByName(deletedPassword);
+    console.log(`Your password ${deletedPassword} is deleted`);
+  }
+
+  if (nextToDo === "update") {
+    console.log("work in progress");
+  }
+
+  // masterpassword in dotenv
+
+  await close();
 }
 
 run();
